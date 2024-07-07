e092de5fffc8d875043f8da2fc860dafd9743609
Jonas Santos <68215037+JonasHenriqueDev@users.noreply.github.com>
2024-07-07 03:23:11 -0300

    feat: configuraçao de CORS

---
+++ src/main/java/br/upe/api/techcycle/marketplace/config/WebConfig.java
@@ -0,0 +1,22 @@
+package br.upe.api.techcycle.marketplace.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("*") // Ou especifique a origem do seu aplicativo Flutter
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
