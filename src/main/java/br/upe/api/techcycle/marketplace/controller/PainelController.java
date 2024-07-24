b5d7d150e3f6b9e2ad9e26f155e09adb9c8b682b
Jonas Santos <68215037+JonasHenriqueDev@users.noreply.github.com>
2024-07-24 01:31:42 -0300

    painel html

--- src/main/java/br/upe/api/techcycle/marketplace/controller/PainelController.java
+++ src/main/java/br/upe/api/techcycle/marketplace/controller/PainelController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 
 @Controller
-@RequestMapping("/painel")
+@RequestMapping("/pontos-coleta")
 public class PainelController {
     @Autowired
     private PontoColetaService pontoColetaService;
