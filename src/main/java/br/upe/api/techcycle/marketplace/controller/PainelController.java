9c2b0f6cc97789c8cf8385e4296f16bae2cdcd18
Jonas Santos <68215037+JonasHenriqueDev@users.noreply.github.com>
2024-07-24 01:27:46 -0300

    painel html

--- src/main/java/br/upe/api/techcycle/marketplace/controller/PainelController.java
+++ src/main/java/br/upe/api/techcycle/marketplace/controller/PainelController.java
@@ -1,14 +1,45 @@
 package br.upe.api.techcycle.marketplace.controller;
 
+import br.upe.api.techcycle.marketplace.dominio.PontoColeta;
+import br.upe.api.techcycle.marketplace.service.PontoColetaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/painel")
 public class PainelController {
+    @Autowired
+    private PontoColetaService pontoColetaService;
+
     @GetMapping
-    public String mostrarPagina() {
-        return "painel"; // Nome do arquivo HTML na pasta templates
+    public String listarPontosColeta(Model model) {
+        List<PontoColeta> pontos = pontoColetaService.findAll();
+        model.addAttribute("pontos", pontos);
+        return "pontos-coleta";
+    }
+
+    @PostMapping("/adicionar")
+    public String adicionarPontoColeta(PontoColeta pontoColeta) {
+        pontoColetaService.save(pontoColeta);
+        return "redirect:/pontos-coleta";
+    }
+
+    @PostMapping("/atualizar")
+    public String atualizarPontoColeta(PontoColeta pontoColeta) {
+        pontoColetaService.save(pontoColeta);
+        return "redirect:/pontos-coleta";
+    }
+
+    @PostMapping("/remover")
+    public String removerPontoColeta(@RequestParam Long id) {
+        pontoColetaService.deleteById(id);
+        return "redirect:/pontos-coleta";
     }
 }
